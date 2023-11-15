76690152cf6e304dc5f24301053ecb35f0a402e5
Hanane <hanane.elkaaba1@gmail.com>
2023-11-15 11:55:53 +0100

    Display name and image of every pokemon

--- PokeAPI/src/App.jsx
+++ PokeAPI/src/App.jsx
@@ -2,20 +2,38 @@ import { useState, useEffect } from "react";
 
 export default function App() {
   const [result, setResult] = useState({});
+  const [pokemonData, setPokemonData] = useState([]);
 
   const fetchData = () => {
-    return fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
       .then((response) => response.json())
-      .then((data) => setResult(data))
-      .catch((err) => console.log(err));
+      .then((result) => {
+        setResult(result);
+        const promises = result.results.map((poke) => {
+          return fetch(poke.url).then((pokeResponse) => pokeResponse.json());
+        });
+        return Promise.all(promises);
+      })
+      .then((pokemonDetails) => {
+        setPokemonData(pokemonDetails);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
-
   useEffect(() => {
     fetchData();
   }, []);
-
+  console.log(pokemonData);
   return (
-    result.results &&
-    result.results.map((poke) => <div key="index">{poke.name}</div>)
+    pokemonData &&
+    pokemonData.map((poke) => (
+      <div key="pokeKey">
+        <a href="">
+          <p>{poke.name}</p>
+          <img src={poke.sprites.back_default} alt="" />
+        </a>
+      </div>
+    ))
   );
 }
